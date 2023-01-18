0fbccf247be6e41abcc14866ad1c6e7d4a760976
Fabio <92931270+FabioRocha2227@users.noreply.github.com>
2023-01-18 18:54:33 +0000

    Final

---
+++ README.md
@@ -0,0 +1,9 @@
+# FEUP-RCOM
+
+* Turma: 3LEIC04
+
+* Carlos Sousa, up202005954
+
+* Daniela Tomás, up202004946
+
+* Fábio Rocha, up202005478
---
+++ proj1/code/include/state_machine.h
@@ -0,0 +1,14 @@
+// Statemachine header.
+
+#ifndef _STATEMACHINE_H_
+#define _STATEMACHINE_H_
+
+extern unsigned char address, control_value, bcc, *data;
+extern unsigned int size;
+
+typedef enum {START, FLAG, ADDRESS, CONTROL, BCC1_OK, BCC2_OK, SUCCESS, ESCAPE, END, REJECT} FState;
+extern FState fstate;
+
+void state_machine(unsigned char byte);
+
+#endif // _STATEMACHINE_H_
